88a9416ce365b83843e4178e859b838b0ab0e832
jjsilveyra <javisilveyra@hotmail.com>
2026-07-23 10:22:47 -0300

    chore(config): allow contentful image domains for next/image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["images.ctfassets.net"],
+    domains: [
+      "images.ctfassets.net",
+      "assets.ctfassets.net",
+      "images.contentful.com",
+    ],
   },
 };
 
